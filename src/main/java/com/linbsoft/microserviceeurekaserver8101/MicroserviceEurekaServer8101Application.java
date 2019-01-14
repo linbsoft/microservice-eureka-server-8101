61ff9da3e861936d60d6038fda16cd2f2aaf205d
linbsoft <gdcxmail@163.com>
2019-01-14 23:58:03 +0800

    spring cloud eureka server

---
+++ src/main/java/com/linbsoft/microserviceeurekaserver8101/MicroserviceEurekaServer8101Application.java
@@ -0,0 +1,16 @@
+package com.linbsoft.microserviceeurekaserver8101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class MicroserviceEurekaServer8101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEurekaServer8101Application.class, args);
+	}
+
+}
+
